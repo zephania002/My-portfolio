f50c3db285915519853b22b6e25e4233c8bb2cf7
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-06-28 08:20:06 +0300

    vue3+ts project scafold

---
+++ src/components/SideBar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+<div class="sidebar-wrapper">
+    
+</div>
+<template>
+
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <p>Home page view</p>
+  </main>
+</template>
