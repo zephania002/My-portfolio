c1d9ab71a78e58be82ec3da2ec7aa474a4bd368e
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-07-03 12:57:21 +0300

    Footer year to current year

--- src/components/FooterVue.vue
+++ src/components/FooterVue.vue
@@ -1,15 +1,22 @@
 
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const currentYear = ref(new Date().getFullYear());
 </script>
 
 <template>
     <footer class="footer">
         <div class="text-center">
-            <small class="copyright"> <i class="fa fa-copyright" aria-hidden="true"></i>wanjala stephen 2022 </small>
+            <small class="copyright">
+                <i class="fa fa-copyright" aria-hidden="true"></i>
+                wanjala stephen {{ currentYear }}
+            </small>
         </div>
     </footer>
-  </template>
+</template>
+
   
   
 
